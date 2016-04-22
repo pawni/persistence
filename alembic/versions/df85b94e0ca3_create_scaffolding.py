e147e4ab0938fa5cc63e9818f7e5d4e0e93b6d9d
Shantanu Singh <shsingh@broadinstitute.org>
2016-04-22 10:30:07 -0400

    Created scaffolding

---
+++ alembic/versions/df85b94e0ca3_create_scaffolding.py
@@ -0,0 +1,24 @@
+"""create scaffolding
+
+Revision ID: df85b94e0ca3
+Revises: 
+Create Date: 2016-04-22 10:29:42.468862
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = 'df85b94e0ca3'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
